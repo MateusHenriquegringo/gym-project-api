a5d372de8fcdc54b72f37ff559552756144e5a89
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2024-05-25 00:09:12 -0300

    improved the logical of the controllers and the service of exercices

--- src/main/java/edu/mateus/Gym/repositorys/ExerciseRepository.java
+++ src/main/java/edu/mateus/Gym/repositorys/ExerciseRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 	List<Exercise> findAllByDifficulty(ExerciseDifficulty difficulty);
+
+	boolean existsByName(String name);
 }
--- src/main/java/edu/mateus/Gym/services/ExerciseService.java
+++ src/main/java/edu/mateus/Gym/services/ExerciseService.java
@@ -1,53 +1,83 @@
 package edu.mateus.Gym.services;
 
+import edu.mateus.Gym.dtos.ExerciseDTO;
 import edu.mateus.Gym.enums.ExerciseDifficulty;
 import edu.mateus.Gym.exceptions.ResourceNotFoundException;
 import edu.mateus.Gym.models.Exercise;
 import edu.mateus.Gym.repositorys.ExerciseRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ExerciseService {
+
 	@Autowired
 	private ExerciseRepository exerciseRepository;
 
-	public List<Exercise> getAllExercices() {
+
+	public List<Exercise> getAllExercises() {
+
 		return exerciseRepository.findAll();
 	}
 
+
 	public Exercise getExerciseById(Long id) {
-		return exerciseRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("exercise not found"));
+
+		return exerciseRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("exercise was not found"));
 	}
+
 
 	@Transactional
-	public Exercise createExercise(Exercise exerciseDTO){
-		return exerciseRepository.save(exerciseDTO);
-	}
+	public Exercise createExercise(ExerciseDTO exerciseDTO) {
 
-	@Transactional
-	public Exercise updateExercise(Exercise exercise) {
-		Exercise ex = this.getExerciseById(exercise.getId());
-		ex.setType(exercise.getType());
-		ex.setName(exercise.getName());
-		ex.setMuscles(exercise.getMuscles());
-		ex.setDifficulty(exercise.getDifficulty());
-		return exerciseRepository.save(ex);
-	}
+		Exercise exercise = new Exercise();
+		BeanUtils.copyProperties(exerciseDTO, exercise);
 
-	public void deleteExercise(Long id) {
-		if(exerciseRepository.existsById(id)){
-		exerciseRepository.deleteById(id);
+		if (exerciseRepository.existsByName(exercise.getName())) {
+			throw new DataIntegrityViolationException("this exercise already exists");
 		} else {
-			throw new ResourceNotFoundException("don't deleted, exercise was not found");
+			return exerciseRepository.save(exercise);
 		}
 	}
 
-	public List<Exercise> getExercisesByDifficulty(ExerciseDifficulty difficulty){
+
+	@Transactional
+	public Exercise editExercise(ExerciseDTO exerciseDTO, Long id) {
+		Exercise exercise = new Exercise();
+		exercise.setId(id);
+		BeanUtils.copyProperties(exerciseDTO, exercise);
+
+
+		if (exerciseRepository.existsById(exercise.getId())) {
+			if (exerciseRepository.existsByName(exercise.getName())) {
+				throw new DataIntegrityViolationException("this exercise already exists");
+			}
+			return exerciseRepository.save(exercise);
+		} else {
+			throw new ResourceNotFoundException("didn't edited, exercise was not found");
+		}
+	}
+
+
+	public void deleteExercise(Long id) {
+
+		if (exerciseRepository.existsById(id)) {
+			exerciseRepository.deleteById(id);
+		} else {
+			throw new ResourceNotFoundException("didn't deleted, exercise was not found");
+		}
+	}
+
+
+	public List<Exercise> getExercisesByDifficulty(ExerciseDifficulty difficulty) {
+
 		return exerciseRepository.findAllByDifficulty(difficulty);
 	}
+
 }
 
