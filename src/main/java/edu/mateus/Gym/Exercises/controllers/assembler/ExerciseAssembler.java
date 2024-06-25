ef8149296c4cfd8ee5796d9dfb02d30751b17ced
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2024-06-25 17:37:29 -0300

    finished controller tests

--- src/main/java/edu/mateus/Gym/Exercises/controllers/assembler/ExerciseAssembler.java
+++ src/main/java/edu/mateus/Gym/Exercises/controllers/assembler/ExerciseAssembler.java
@@ -4,7 +4,6 @@ import edu.mateus.Gym.Exercises.controllers.ExerciseController;
 import edu.mateus.Gym.Exercises.models.ExerciseModel;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
-import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -24,14 +23,17 @@ public class ExerciseAssembler extends RepresentationModelAssemblerSupport<Exerc
 	public ExerciseModel toModel(ExerciseModel entity) {
 
 		entity
-				.add(linkTo(methodOn(ExerciseController.class).getExerciseById(entity.getId())).withSelfRel());
+				.add(linkTo(methodOn(ExerciseController.class).getExerciseById(entity.getId())).withSelfRel())
+				.add(linkTo(methodOn(ExerciseController.class).getAllExercises(null, null)).withRel("allExercises"));
 		return entity;
 	}
 
+
 	@Override
-	public CollectionModel <ExerciseModel> toCollectionModel(Iterable<? extends ExerciseModel> entities) {
+	public CollectionModel<ExerciseModel> toCollectionModel(Iterable<? extends ExerciseModel> entities) {
 
 		return super.toCollectionModel(entities);
+
 	}
 
 
--- src/test/java/edu/mateus/Gym/Exercises/controllers/ExerciseControllerTest.java
+++ src/test/java/edu/mateus/Gym/Exercises/controllers/ExerciseControllerTest.java
@@ -1,12 +1,15 @@
 package edu.mateus.Gym.Exercises.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.mateus.Gym.Exercises.controllers.assembler.ExerciseAssembler;
+import edu.mateus.Gym.Exercises.dtos.ExerciseDTO;
 import edu.mateus.Gym.Exercises.enums.ExerciseDifficulty;
 import edu.mateus.Gym.Exercises.enums.ExerciseIntensity;
 import edu.mateus.Gym.Exercises.enums.ExerciseType;
 import edu.mateus.Gym.Exercises.enums.MuscleGroupsEnum;
 import edu.mateus.Gym.Exercises.models.ExerciseModel;
 import edu.mateus.Gym.Exercises.services.ExerciseService;
-import edu.mateus.Gym.Exercises.services.assembler.ExerciseAssembler;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -17,6 +20,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -27,6 +32,7 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = ExerciseController.class)
@@ -34,6 +40,9 @@ public class ExerciseControllerTest {
 
 	@Autowired
 	MockMvc mvc;
+
+	@Resource
+	ObjectMapper mapper;
 
 	@MockBean
 	ExerciseService service;
@@ -44,27 +53,32 @@ public class ExerciseControllerTest {
 	@Captor
 	ArgumentCaptor<ExerciseDifficulty> difficultyArgumentCaptor;
 
+	@Captor
+	ArgumentCaptor<Long> longArgumentCaptor;
+
+	@Captor
+	ArgumentCaptor<List<MuscleGroupsEnum>> musclesArgumentCaptor;
+
 	@InjectMocks
 	ExerciseController controller;
 
-	List<ExerciseModel> exerciseModelList = List.of(
-			ExerciseModel.builder()
-					.id(1L)
-					.difficulty(ExerciseDifficulty.INICIANTE)
-					.name("test")
-					.intensity(ExerciseIntensity.BAIXA)
-					.muscles(List.of(MuscleGroupsEnum.TRICEPS))
-					.type(ExerciseType.CARDIO)
-					.build(),
-			ExerciseModel.builder()
-					.id(1L)
-					.difficulty(ExerciseDifficulty.INTERMEDIARIO)
-					.name("test two")
-					.intensity(ExerciseIntensity.BAIXA)
-					.muscles(List.of(MuscleGroupsEnum.TRICEPS))
-					.type(ExerciseType.CARDIO)
-					.build()
-	);
+	List<ExerciseModel> exerciseModelList = List.of(ExerciseModel.builder()
+			                                                .id(1L)
+			                                                .difficulty(ExerciseDifficulty.INICIANTE)
+			                                                .name("test")
+			                                                .intensity(ExerciseIntensity.BAIXA)
+			                                                .muscles(List.of(MuscleGroupsEnum.TRICEPS,
+			                                                                 MuscleGroupsEnum.BICEPS))
+			                                                .type(ExerciseType.CARDIO)
+			                                                .build(), ExerciseModel.builder()
+			                                                .id(1L)
+			                                                .difficulty(ExerciseDifficulty.INTERMEDIARIO)
+			                                                .name("test two")
+			                                                .intensity(ExerciseIntensity.BAIXA)
+			                                                .muscles(List.of(MuscleGroupsEnum.TRICEPS,
+			                                                                 MuscleGroupsEnum.PEITORAL))
+			                                                .type(ExerciseType.CARDIO)
+			                                                .build());
 
 
 	@Nested
@@ -87,10 +101,7 @@ public class ExerciseControllerTest {
 			verify(service, never()).getAllExercises(any(), any());
 
 		}
-		@Test
-		void testAssemblerHATEOAS() throws Exception {
 
-		}
 
 		@Test
 		@DisplayName("getAllExercises should work correctly")
@@ -120,8 +131,7 @@ public class ExerciseControllerTest {
 			var difficulty = ExerciseDifficulty.INTERMEDIARIO;
 
 			var filteredByDifficulty = exerciseModelList.stream()
-					.filter(exerciseModel -> exerciseModel.getDifficulty()
-							.equals(difficulty))
+					.filter(exerciseModel -> exerciseModel.getDifficulty().equals(difficulty))
 					.toList();
 
 			var exerciseModelsCollection = CollectionModel.of(filteredByDifficulty);
@@ -131,16 +141,14 @@ public class ExerciseControllerTest {
 			when(assembler.toCollectionModel(anyList())).thenReturn(exerciseModelsCollection)
 					.thenReturn(exerciseModelsCollection);
 
-			mvc.perform(get("/api/exercises")
-					            .param("difficulty", String.valueOf(difficulty)))
-					.andExpect(jsonPath("$._embedded.exerciseModelList[0].name").value("test two"))
-					.andExpect(
-							jsonPath("$._embedded.exerciseModelList[0].id").value(filteredByDifficulty.get(0).getId()))
-					.andExpect(jsonPath("$._embedded.exerciseModelList[0].muscles").isNotEmpty())
-					.andExpect(jsonPath("$._embedded.exerciseModelList[0].difficulty").value(
+			mvc.perform(get("/api/exercises").param("difficulty", String.valueOf(difficulty)))
+					.andExpect(jsonPath("$._embedded.exercises[0].name").value("test two"))
+					.andExpect(jsonPath("$._embedded.exercises[0].id").value(filteredByDifficulty.get(0).getId()))
+					.andExpect(jsonPath("$._embedded.exercises[0].muscles").isNotEmpty())
+					.andExpect(jsonPath("$._embedded.exercises[0].difficulty").value(
 							difficultyArgumentCaptor.getValue().toString()))
 					.andExpect(content().contentType("application/hal+json"))
-					.andExpect(jsonPath("$._embedded.exerciseModelList.size()").value(filteredByDifficulty.size()))
+					.andExpect(jsonPath("$._embedded.exercises.size()").value(filteredByDifficulty.size()))
 					.andExpect(status().is(200));
 
 
@@ -150,7 +158,127 @@ public class ExerciseControllerTest {
 			verify(service, times(1)).getAllExercises(any(), any());
 		}
 
+
+		@Test
+		@DisplayName("getAllExercises should capture the difficulty param and return correctly")
+		void testGetAllExercisesWithMusclesParam() throws Exception {
+
+			var muscles = List.of(MuscleGroupsEnum.PEITORAL);
+
+			var filteredByMuscles = exerciseModelList.stream()
+					.filter(exerciseModel -> exerciseModel.getMuscles().containsAll(muscles))
+					.toList();
+
+			var exerciseModelsCollection = CollectionModel.of(filteredByMuscles);
+
+			when(service.getAllExercises(eq(null), musclesArgumentCaptor.capture())).thenReturn(filteredByMuscles);
+			when(assembler.toCollectionModel(anyList())).thenReturn(exerciseModelsCollection);
+
+			var output = mvc.perform(get("/api/exercises").param("muscles", muscles.get(0).toString()))
+					.andExpect(jsonPath("$._embedded.exercises[0].name").value("test two"))
+					.andExpect(jsonPath("$._embedded.exercises[0].muscles").isNotEmpty())
+					.andExpect(jsonPath("$._embedded.exercises[0].id").value(filteredByMuscles.get(0).getId()))
+					.andExpect(content().contentType("application/hal+json"))
+					.andExpect(jsonPath("$._embedded.exercises.size()").value(filteredByMuscles.size()))
+					.andExpect(status().is(200))
+					.andReturn();
+
+
+			System.out.println(output.getResponse().getContentAsString());
+			assertTrue(musclesArgumentCaptor.getValue().containsAll(muscles));
+
+			verify(assembler, times(1)).toCollectionModel(any());
+			verify(service, times(1)).getAllExercises(any(), any());
+		}
+
+
 	}
 
+	@Nested
+	public class createExercise {
+
+		@Test
+		void shouldCreateAnExerciseSuccessfully() throws Exception {
+
+			var dto = ExerciseDTO.builder()
+					.name("nome")
+					.muscles(List.of(MuscleGroupsEnum.TRICEPS))
+					.intensity(ExerciseIntensity.BAIXA)
+					.difficulty(ExerciseDifficulty.INICIANTE).type(ExerciseType.CARDIO).build();
+
+
+			when(service.createExercise(any(ExerciseDTO.class))).thenReturn(exerciseModelList.get(0));
+			when(assembler.toModel(any(ExerciseModel.class))).thenReturn(exerciseModelList.get(0));
+
+			mvc.perform(post("/api/exercises")
+					            .contentType(MediaType.APPLICATION_JSON)
+					            .content(mapper.writeValueAsString(dto)))
+					.andExpect(status().isCreated())
+					.andExpect(jsonPath("$.name").value("test"))
+					.andReturn();
+
+			verify(assembler, times(1)).toModel(any());
+			verify(service, times(1)).createExercise(any());
+
+		}
+
+
+		@Test
+		void shouldNotCreateAnExerciseWithIncorrectParams() throws Exception {
+
+			var dto = ExerciseDTO.builder()
+					.name("")
+					.build();
+
+			when(service.createExercise(any(ExerciseDTO.class))).thenReturn(exerciseModelList.get(0));
+			when(assembler.toModel(any(ExerciseModel.class))).thenReturn(exerciseModelList.get(0));
+
+			mvc.perform(post("/api/exercises")
+					            .contentType(MediaType.APPLICATION_JSON)
+					            .content(mapper.writeValueAsString(dto)))
+					.andExpect(status().isBadRequest());
+
+			verify(service, never()).createExercise(any());
+		}
+
+
+		@Nested
+		public class getExerciseById {
+
+			@Test
+			void getByIdWorkSuccessfully() throws Exception {
+
+				when(service.getExerciseById(longArgumentCaptor.capture())).thenReturn(exerciseModelList.get(0));
+				when(assembler.toModel(any(ExerciseModel.class))).thenReturn(exerciseModelList.get(0));
+
+				mvc.perform(get("/api/exercises/{id}", 1L))
+						.andExpect(status().isOk())
+						.andExpect(content().contentType("application/hal+json"))
+						.andExpect(jsonPath("$.id").value(longArgumentCaptor.getValue()))
+						.andReturn();
+
+
+				verify(service, times(1)).getExerciseById(longArgumentCaptor.getValue());
+				verify(assembler, times(1)).toModel(any(ExerciseModel.class));
+				assertEquals(longArgumentCaptor.getValue(), 1L);
+
+			}
+
+
+			@Test
+			void getByIdWhenIdDoesNotExists() throws Exception {
+
+				Long id = 9000L;
+
+				var output = mvc.perform(get("/api/exercise/{id}", id))
+						.andExpect(status().isNotFound())
+						.andReturn();
+
+			}
+
+		}
+
+
+	}
 
 }
